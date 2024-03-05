15b122c2d22d3ce302474662fc203421b8b6d04c
TamNhien <canhthien91@gmail.com>
2024-03-05 20:13:13 +0700

    Add project files.

---
+++ LTCB_InPhanTuDauTienDayFibonacci.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1, c;
+
+    cout << "Nhap so nguyen duong n: ";
+    cin >> n;
+
+    cout << "Cac phan tu dau tien cua day Fibonacci la: ";
+
+    for (int i = 0; i < n; ++i) {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
